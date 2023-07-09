5e23bc0cbdf95bf1514407f1bd3e2b5ef1968c10
Mohammad Rabie <mmohhamadd@gmail.com>
2023-07-09 15:41:34 +0330

    server structure initialization

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run()
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr)
+    }
+}
